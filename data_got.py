014b71ed4092ff567424ea12468ec89de3179f20
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-03-27 11:47:43 +0800

    update

--- data_got.py
+++ data_got.py
@@ -2,13 +2,17 @@ import numpy as np
 from mxnet.contrib import text
 import torch.utils.data as data_utils
 import torch
+import pickle as pkl
+
 def load_data(batch_size=64):
-    X_tst = np.load("/data/blchen/text/preprocessed/AAPD/X_test.npy")
-    X_trn = np.load("/data/blchen/text/preprocessed/AAPD/X_train.npy")
-    Y_trn = np.load("/data/blchen/text/preprocessed/AAPD/y_train.npy")
-    Y_tst = np.load("/data/blchen/text/preprocessed/AAPD/y_test.npy")
-    label_embed = np.load("/data/blchen/text/preprocessed/AAPD/label_embed.npy")
-    embed = text.embedding.CustomEmbedding('/data/blchen/text/preprocessed/AAPD/word_embed.txt')
+    X_tst = np.load("../../datasets/ProgrammerWeb/npy_data/test_texts.npy")
+    X_trn = np.load("../../datasets/ProgrammerWeb/npy_data/train_texts.npy")
+    Y_trn = np.load("../../datasets/ProgrammerWeb/npy_data/train_labels.npy")
+    Y_tst = np.load("../../datasets/ProgrammerWeb/npy_data/test_labels.npy")
+    # label_embed = np.load("../../datasets/ProgrammerWeb/npy_data/label_embed.npy")
+    with open('../../datasets/ProgrammerWeb/word_embedding_model/label_embed.npy', 'rb') as pkl_file:
+        label_embed = pkl.load(pkl_file)
+    embed = text.embedding.CustomEmbedding('../../datasets/ProgrammerWeb/npy_data/word_embed.txt')
     train_data = data_utils.TensorDataset(torch.from_numpy(X_trn).type(torch.LongTensor),
                                           torch.from_numpy(Y_trn).type(torch.LongTensor))
     test_data = data_utils.TensorDataset(torch.from_numpy(X_tst).type(torch.LongTensor),
